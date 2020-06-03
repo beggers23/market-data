8e63c08196909eacd1553600fec73486661cccf1
Brendan Eggers <brendan.eggers@openslate.com>
2020-06-03 10:25:29 -0400

    Grabs market data from rapidAPI and renders small snippet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+
+const { API_URL, API_HOST, API_KEY } = window;
 
 function App() {
+  const [marketData, setMarketData] = useState([]);
+  useEffect(() => {
+    const grabMarketData = async () => {
+      const req = await fetch(API_URL, {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": API_HOST,
+          "x-rapidapi-key": API_KEY,
+          useQueryString: true,
+        },
+      });
+      const res = await req.json();
+      if (res) {
+        setMarketData(res.marketSummaryResponse.result);
+      }
+    };
+    grabMarketData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {marketData.map((ticker) => {
+        const positive = ticker.regularMarketChangePercent.raw > 0;
+        return (
+          <div key={ticker.symbol} className="tickerDataWrapper">
+            <span>{ticker.shortName}</span>{" "}
+            <span>{ticker.regularMarketPrice.fmt}</span>{" "}
+            <span className={positive ? "green" : "red"}>
+              {positive && "+"}
+              {ticker.regularMarketChangePercent.fmt}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
